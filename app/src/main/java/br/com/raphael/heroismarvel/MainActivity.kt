aff789aa473dfbf8df1e64f27f42978442cf4ee4
Raphael Marques <raphael.marques@nuvem.net>
2020-04-19 01:23:34 -0400

    ISSUE-15 - Adicionar Splash screen e mudança da cor do theme

--- app/src/main/java/br/com/raphael/heroismarvel/MainActivity.kt
+++ app/src/main/java/br/com/raphael/heroismarvel/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
